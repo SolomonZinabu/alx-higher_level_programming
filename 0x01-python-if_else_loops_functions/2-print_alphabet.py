f0354614cd6d0d7c0c01a2d4e434445a355b6607
Solomon Zinabu <49882150+SolomonZinabu@users.noreply.github.com>
2022-10-18 10:03:00 +0300

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-<<<<<<< HEAD
-
-for i in range(97, 123):
-    print("{:c}".format(i), end="")
-=======
-for a in "abcdefghijklmnopqrstuvwxyz":
-    print('{}'.format(a), end="")
->>>>>>> 46949fe5db0c6176bf1acc5e0d855f40e65f9c38
--- 0x01-python-if_else_loops_functions/README.md
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-This directory contains programs covering if, else & loops concepts as implemented in Pytyhon among many others.
-=======
-this is python part 2 of the assigment
->>>>>>> 46949fe5db0c6176bf1acc5e0d855f40e65f9c38
