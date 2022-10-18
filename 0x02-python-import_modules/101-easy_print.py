0919bb7f073d8916f6938fd3d8fb9d987e871124
Solomon Zinabu <49882150+SolomonZinabu@users.noreply.github.com>
2022-10-18 10:03:17 +0300

    Delete 0x02-python-import_modules directory

--- 0x02-python-import_modules/101-easy_print.py
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
-exec('import subprocess; subprocess.call(["echo", "#pythoniscool"])')
-=======
-#!usr/bin/python3
-open(1, 'w').write("#pythoniscool\n")
->>>>>>> 46949fe5db0c6176bf1acc5e0d855f40e65f9c38
